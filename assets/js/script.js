e72971c52ffac8d83ea302c00b693ab27c31f99a
codershakibs <codershakibs@gmail.com>
2024-02-28 22:22:08 +0600

    first commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,20 @@
-// Portfolio image animation slide - 3 -/Start
 document.addEventListener("DOMContentLoaded", function () {
-    let splideElements = document.querySelectorAll("#portfolio-third-slide");
+    let splideElements = document.querySelectorAll(".explore-proggress", {
+        focus: 0.8,
+        trimSpace: false,
+        perPage: 4,
+        perMove: 4,
+        gap: "30px",
 
-    splideElements.forEach(function (element) {
-        let splide = new Splide(element, {
-            perPage: 3,
-            perMove: 1,
-            height: '9rem',
-            focus: 'center',
-            trimSpace: false,
-        });
 
-        splide.mount({ URLHash });
     });
+
+    var splide = new Splide('.splide');
+    var bar = splide.root.querySelector('.my-slider-progress-bar');
+    splide.on('mounted move', function () {
+        var end = splide.Components.Controller.getEnd() + 1;
+        var rate = Math.min((splide.index + 1) / end, 1);
+        bar.style.width = String(100 * rate) + '%';
+    });
+    splide.mount();
 });
-// Portfolio image animation slide -3 -/End
