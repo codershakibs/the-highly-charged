adfe88a020db396626b018afb9abbb4a2f914482
codershakibs <codershakibs@gmail.com>
2024-02-29 03:45:15 +0600

    first commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,18 @@
 document.addEventListener("DOMContentLoaded", function () {
-    let splideElements = document.querySelectorAll(".explore-proggress", {
-        focus: 0.8,
+    let splideElements = document.querySelectorAll(".explore-proggress");
+
+    var splide = new Splide('.splide', {
+        focus: 0.64,
         trimSpace: false,
-        perPage: 4,
+        perPage: 5,
         perMove: 4,
         gap: "30px",
-
+        updateOnMove: true,
+        pagination: false,
+        // autoplay: true,
+        speed: 2500,
 
     });
-
-    var splide = new Splide('.splide');
     var bar = splide.root.querySelector('.my-slider-progress-bar');
     splide.on('mounted move', function () {
         var end = splide.Components.Controller.getEnd() + 1;
